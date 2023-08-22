fb6ff75b9779582de50c2c075b6bbe80e57972c7
Simeon Marchand <simeonrmarchand@gmail.com>
2023-08-21 19:03:57 -0500

    added the tabs for add, search, and orders

--- src/App.java
+++ src/App.java
@@ -3,6 +3,7 @@ import java.awt.*;
 
 public class App {
     private final JFrame frame; // Main application window
+    private JTabbedPane tabbedPane; // Tabbed pane to hold the tabs
     private JTextField itemNameField; // Text field for item name
     private JTextField categoryField; // Text field for item category
     private JTextField descriptionField; // Text field for item description
@@ -15,41 +16,41 @@ public class App {
         frame = new JFrame("StreamlineWare"); // Create the main application window
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Set close operation
 
-        initializeComponents(); // Initialize GUI components
+        initializeComponents(createAddTabContent(), createSearchTabContent(), createOrdersTabContent()); // Initialize the GUI components
         setupLayout(); // Set up the layout of GUI components
 
         frame.pack(); // Pack the components within the frame
         frame.setVisible(true); // Make the frame visible
     }
 
-    private void initializeComponents() {
-        itemNameField = new JTextField(20); // Initialize the item name field
-        categoryField = new JTextField(20); // Initialize the item category field
-        descriptionField = new JTextField(20); // Initialize the item description field
-        unitPriceField = new JTextField(20); // Initialize the unit price field
-        quantityInStockField = new JTextField(20); // Initialize the quantity in stock field
-        minimumStockLevelField = new JTextField(20); // Initialize the minimum stock level field
+    private void initializeComponents(JPanel createAddTabContent, JPanel createSearchTabContent, JPanel createOrdersTabContent) {
+        tabbedPane = new JTabbedPane(); // Initialize the tabbed pane
+        tabbedPane.addTab("Add Item", createAddTabContent); // Add the add item tab
+        tabbedPane.addTab("Search Items", createSearchTabContent); // Add the search items tab
+        tabbedPane.addTab("Orders Items", createOrdersTabContent); // Add the view items tab
         addButton = new JButton("Add Item"); // Initialize the add button
     }
 
     private void setupLayout() {
-        JPanel panel = new JPanel(new GridLayout(7, 2, 10, 10)); // Create a panel with a grid layout
-        panel.add(new JLabel("Item Name:")); // Add labels and corresponding input fields
-        panel.add(itemNameField);
-        panel.add(new JLabel("Category:"));
-        panel.add(categoryField);
-        panel.add(new JLabel("Description:"));
-        panel.add(descriptionField);
-        panel.add(new JLabel("Unit Price:"));
-        panel.add(unitPriceField);
-        panel.add(new JLabel("Quantity in Stock:"));
-        panel.add(quantityInStockField);
-        panel.add(new JLabel("Minimum Stock Level:"));
-        panel.add(minimumStockLevelField);
-        panel.add(new JLabel()); // Empty label for spacing
-        panel.add(addButton); // Add the add button
+        frame.add(tabbedPane); // Add the tabbed pane to the frame
+    }
 
-        frame.add(panel); // Add the panel to the frame
+    private JPanel createAddTabContent(){
+        JPanel addTabPanel = new JPanel(new GridLayout(7, 2, 10, 10)); // Create a panel with 7 rows, 2 columns, and 10px horizontal and vertical gaps
+        // add UI components specific to the add tab
+        return addTabPanel;
+    }
+
+    private JPanel createSearchTabContent(){
+        JPanel searchTabPanel = new JPanel();
+        // add UI components specific to the search tab
+        return searchTabPanel;
+    }
+
+    private JPanel createOrdersTabContent(){
+        JPanel ordersTabPanel = new JPanel();
+        // add UI components specific to the orders tab
+        return ordersTabPanel;
     }
 
     // Getter methods to access GUI components
