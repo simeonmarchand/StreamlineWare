7ec6d65b526015043a0259905596fcfa196b03c6
Simeon Marchand <simeonrmarchand@gmail.com>
2023-08-29 15:46:14 -0500

    Need to fix the InsertStatement

--- src/com/simeonmarchand/streamlineware/ui/PlaceOrderForm.java
+++ src/com/simeonmarchand/streamlineware/ui/PlaceOrderForm.java
@@ -1,11 +1,16 @@
 package com.simeonmarchand.streamlineware.ui;
 
+import com.simeonmarchand.streamlineware.data.DatabaseConnection;
 import com.simeonmarchand.streamlineware.logger.InventoryLogger;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class PlaceOrderForm extends JFrame {
     private JTextField itemnameField;
@@ -14,6 +19,7 @@ public class PlaceOrderForm extends JFrame {
     private JTextField customerNameField;
     private JTextField customerEmailField;
     private JTextField orderNotesField;
+    private int requestedQuantity;
     
     public PlaceOrderForm(){
         setTitle("Place Order");
@@ -59,12 +65,74 @@ public class PlaceOrderForm extends JFrame {
                 String orderNotes = orderNotesField.getText();
                 
                 // log the input values
-                InventoryLogger.logInfo("This information is being logged\n" + " Item Name: " + itemName + " Quantity" +
-                        " " + quantity + " Order Date" + orderDate + " Customer Name " + customerName + " Customer Email " + customerEmail + " Order Notes " + orderNotes);
+                if (itemName.isEmpty()){
+                    InventoryLogger.logError("Item Name is empty");
+                } else
+                    InventoryLogger.logInfo("This information is being logged\n" + " Item Name: " + itemName +
+                        " Quantity: \n" + quantity + " Order Date: \n" + orderDate + " Customer Name: \n" + customerName +
+                        "Customer Email: \n" + customerEmail + " Order Notes \n" + orderNotes);
                 
                 // update the database
                 // add the order
                 // display a success/failure message
+                String checkStockQuery = "SELECT quantity_in_stock FROM items WHERE name = ?";
+                
+                try (Connection connection = DatabaseConnection.getConnection()) {
+                    PreparedStatement checkStockStatement = connection.prepareStatement(checkStockQuery);
+                    checkStockStatement.setString(1, itemName);
+                    ResultSet stockResult = checkStockStatement.executeQuery();
+                    
+                    if (stockResult.next()) {
+                        requestedQuantity = Integer.parseInt(quantity);
+                        int currentStock = stockResult.getInt("quantity_in_stock");
+                        
+                        if (currentStock >= requestedQuantity){
+                            String getItemIDQuery = "SELECT items_id FROM items WHERE name = ?";
+                            String updateStockQuery = "UPDATE items SET quantity_in_stock = ? WHERE name = ?";
+                            String insertOrderQuery = "INSERT INTO orders (item_name, quantity, order_date, customer_name, customer_email, order_notes) VALUES (?, ?, ?, ?, ?, ?)";
+                            
+                            try(PreparedStatement getItemIDStatement = connection.prepareStatement(getItemIDQuery);
+                                PreparedStatement updateStockStatement = connection.prepareStatement(updateStockQuery);
+                                PreparedStatement insertOrderStatement = connection.prepareStatement(insertOrderQuery)){
+                                
+                                // get the item ID
+                                getItemIDStatement.setString(1, itemName);
+                                ResultSet itemIDResult = getItemIDStatement.executeQuery();
+                                
+                                if (itemIDResult.next()){
+                                    int itemID = itemIDResult.getInt("items_id");
+                                    
+                                    //update the stock WORKING
+                                    int updatedStock = currentStock - requestedQuantity;
+                                    updateStockStatement.setInt(1, updatedStock);
+                                    updateStockStatement.setString(2, itemName);
+                                    updateStockStatement.executeUpdate();
+                                    InventoryLogger.logInfo("Stock updated Successfully");
+                                    
+                                    // TODO: Insert order needs to be fixed
+                                    // Think I need to add the item ID to the insert order statement
+                                    customerNameField.getText();
+                                    customerEmailField.getText();
+                                    insertOrderStatement.setInt(2, requestedQuantity);
+                                    insertOrderStatement.setString(3, orderDate);
+                                    insertOrderStatement.setString(4, customerName);
+                                    insertOrderStatement.setString(5, customerEmail);
+                                    insertOrderStatement.setString(6, orderNotes);
+                                    insertOrderStatement.executeUpdate();
+                                    InventoryLogger.logInfo("Order added Successfully");
+                                    
+                                    JOptionPane.showInputDialog(PlaceOrderForm.this, "Order Placed Successfully");
+                                } else {
+                                    JOptionPane.showMessageDialog(PlaceOrderForm.this, "Item not found");
+                                }
+                            } catch (SQLException ex) {
+                                throw new RuntimeException(ex);
+                            }
+                        }
+                    }
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
                 
                 JOptionPane.showMessageDialog(null, "Order Placed Successfully");
                 dispose();
